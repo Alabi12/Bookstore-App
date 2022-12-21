1b6cb091e3f5ecfac57a1ec1b1074b31a4a716d5
Alabi12 <wolo22001@gmail.com>
2022-12-21 15:39:57 +0000

    update the file names in the src folder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import BookList from './components/BookList';
-import NavBar from './components/navBar';
+import NavBar from './components/NavBar';
 import Categories from './components/categories';
-import Footer from './components/footer';
+import Footer from './components/Footer';
 
 const App = () => (
   <div className="App">
