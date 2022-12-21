5cb89bdbbcfcf72f540156e86d156c9c91a7dda3
Alabi12 <wolo22001@gmail.com>
2022-12-21 12:21:31 +0000

    correct linter error

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/navBar';
 import Categories from './components/categories';
 import Footer from './components/footer';
-import HomePage from './components/homePage';
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<BookList />} />
         <Route path="/categories" element={<Categories />} />
       </Routes>
       <Footer />
